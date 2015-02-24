47a94f7df0b085c9a1927d19c06a0dc4eee199c6
Ronald Hernandez <Ronald@neulynx.com>
2015-02-24 04:31:11 -0500

    added form validations for sign up

--- NeuLynx/SignUpViewController.swift
+++ NeuLynx/SignUpViewController.swift
@@ -25,17 +25,45 @@ class SignUpViewController: UIViewController {
         
         var error = ""
         
-        if email.text == "" || password.text == "" {
-            error = "Please enter a username an password"
+        
+        if email.text == "" || password.text == "" || confirmPassword.text == "" {
+            //make the borders of each field red if any of them are empty
+            email.layer.cornerRadius = 8
+             email.layer.borderWidth = 2
+            email.layer.borderColor = UIColor.redColor().CGColor
             
+            password.layer.cornerRadius = 8
+            password.layer.borderWidth = 2
+            password.layer.borderColor = UIColor.redColor().CGColor
+            
+            confirmPassword.layer.cornerRadius = 8
+            confirmPassword.layer.borderWidth = 2
+            confirmPassword.layer.borderColor = UIColor.redColor().CGColor
+            
+            error = "Please enter a username an password"
+           
+        } else if isValidEmail(email.text) == false{
+                email.layer.cornerRadius = 8
+                email.layer.borderWidth = 2
+                email.layer.borderColor = UIColor.redColor().CGColor
+                email.text = "Please enter a valid email"
+                
+                
+                
+            } else {
+                email.layer.cornerRadius = 8
+                email.layer.borderWidth = 2
+                email.layer.borderColor = UIColor.greenColor().CGColor
         }
         //set up alert and display to user
         if error != "" {
+            
             var alert = UIAlertController(title: "Error in Register Form", message: error, preferredStyle: UIAlertControllerStyle.Alert)
             alert.addAction(UIAlertAction(title: "OK", style: .Default, handler:{
                 action  in
                self.dismissViewControllerAnimated(true, completion: nil)
             }))
+            
             self.presentViewController(alert, animated: true, completion: nil)
         }
         
@@ -45,6 +73,8 @@ class SignUpViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+      
     }
 
     override func didReceiveMemoryWarning() {
@@ -52,6 +82,15 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    func isValidEmail(testStr:String) -> Bool {
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let range = testStr.rangeOfString(emailRegEx, options:.RegularExpressionSearch)
+        let result = range != nil ? true : false
+        return result
+        
+    }
 
     /*
     // MARK: - Navigation
